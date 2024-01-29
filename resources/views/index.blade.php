939b553856274b359a693ed7754eda6ff3daff8c
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-01-29 09:05:52 +0700

    hm

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -44,7 +44,6 @@
                 <ul>
                     <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
                     <li><a class="nav-link scrollto" href="#about">About</a></li>
-                    <li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
                     <li><a class="nav-link scrollto" href="#team">Team</a></li>
                     <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
                 </ul>
